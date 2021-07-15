38aef8fc008303f0310e2c57cfc7b220eda5ed4f
Jan Hartman <janhartman@eleven.nl>
2021-07-15 14:26:24 +0200

    175. Assignment - Write Test for IndexController (Using JUnit5)

--- src/test/java/guru/springframework/recipe/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/recipe/controllers/IndexControllerTest.java
@@ -1,17 +1,22 @@
 package guru.springframework.recipe.controllers;
 
+import guru.springframework.recipe.domain.Recipe;
 import guru.springframework.recipe.services.RecipeServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Jan Hartman
@@ -28,6 +33,17 @@ class IndexControllerTest {
 
 	@Test
 	void getIndexPage() {
+		// given
+		Set<Recipe> recipes = new HashSet<>();
+		final Recipe recipeA = new Recipe();
+		recipeA.setId(1L);
+		recipes.add(recipeA);
+		final Recipe recipeB = new Recipe();
+		recipeB.setId(2L);
+		recipes.add(recipeB);
+
+		when(recipeService.getAllRecipes()).thenReturn(recipes);
+		ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
 
 		// When
 		final String indexPage = indexController.getIndexPage(model);
@@ -35,7 +51,10 @@ class IndexControllerTest {
 		// Then
 		assertThat(indexPage).isEqualTo("index");
 		verify(recipeService, times(1)).getAllRecipes();
-		verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+		verify(model, times(1))
+				.addAttribute(eq("recipes"), argumentCaptor.capture());
+		Set<Recipe> setCapuredIndexController = argumentCaptor.getValue();
+		assertThat(setCapuredIndexController.size()).isEqualTo(recipes.size());
 	}
 
 	@BeforeEach
