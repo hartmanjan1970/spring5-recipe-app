46280862e67761f8c9a8d8d20676b1fb74f01614
Jan Hartman <janhartman@eleven.nl>
2021-07-15 15:02:43 +0200

    178. Introduction to Spring MockMVC

--- src/test/java/guru/springframework/recipe/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/recipe/controllers/IndexControllerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -17,6 +19,9 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 /**
  * @author Jan Hartman
@@ -62,5 +67,14 @@ class IndexControllerTest {
 		MockitoAnnotations.openMocks(this);
 
 		indexController = new IndexController(recipeService);
+	}
+
+	@Test
+	void testMockMVC() throws Exception {
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+		mockMvc.perform(get("/"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("index"));
 	}
 }
